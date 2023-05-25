0c5c94d99d9b0a4df047382a7bbe795efd032813
Jason-77777 <126946180+Jason-77777@users.noreply.github.com>
2023-05-25 10:07:47 +0700

    push code
    personal

--- PBO/src/App.java
+++ PBO/src/App.java
@@ -1,8 +1,5 @@
-//NAMA : JASON
-//NIM : 03081220018
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ini projek PBO");
+        System.out.println("Hello, World!");
     }
 }
