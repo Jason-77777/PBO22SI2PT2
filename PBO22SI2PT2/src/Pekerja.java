e1b6d68aefd2fb25d206c1ff762cc9747b1b96b2
GeovaniZovintho <geovanizovintho@gmail.com>
2023-05-27 15:16:46 +0700

    Update Pekerja.java

--- PBO22SI2PT2/src/Pekerja.java
+++ PBO22SI2PT2/src/Pekerja.java
@@ -1,7 +1,5 @@
 //Pekerja adalah sebuah badan usaha yang melibatkan hubungan antara atasan dengan pekerja/karyawan.
+// isinya adalah data yang menyangkut pekerja seperti data diri, alamat, dll
 
 public class Pekerja {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Ini projek PBO");
-    }
 }
