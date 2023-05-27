34f2a29dbaa5b76bcc51c4d1c6958fa72c942b69
VickyDarmana <134504626+VickyDarmana@users.noreply.github.com>
2023-05-27 14:26:17 +0700

    Update VickyDarmana.java

---
+++ PBO22SI2PT2/src/VickyDarmana.java
@@ -0,0 +1,8 @@
+//NAMA : Vicky Darmana
+//NIM : 03081220038
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ini projek PBO");
+    }
+}
