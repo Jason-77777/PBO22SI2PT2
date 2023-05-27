a17ead6af5e1684d6adc761cf778afe50eeaaa02
Jason-77777 <126946180+Jason-77777@users.noreply.github.com>
2023-05-27 14:28:35 +0700

    jason
    java

---
+++ PBO22SI2PT2/src/jason.java
@@ -0,0 +1,8 @@
+//NAMA :JASON
+//NIM :03081220018
+
+public class jason {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ini projek PBO, halo nama saya jason");
+    }
+}
