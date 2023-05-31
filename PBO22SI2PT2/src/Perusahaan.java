970de01e21b76ef2252652c9e2ac995066348719
GeovaniZovintho <geovanizovintho@gmail.com>
2023-05-31 20:35:17 +0700

    Update Perusahaan.java
    setter getter

--- PBO22SI2PT2/src/Perusahaan.java
+++ PBO22SI2PT2/src/Perusahaan.java
@@ -1,35 +1,36 @@
 //Perusahaan adalah sebuah badan usaha yang tetap dan didirikan oleh manusia yang bertujuan mencari laba atau keuntungan
 
 public class Perusahaan {
-    private String namaperusahaan;
-    private String alamatperusahaan;
-    private String dataperusahaan;
+    private String nama_perusahaan;
+    private String alamat_perusahaan;
+    private String data_perusahaan;
     private String pekerja;
     private String investor;
     
 
-    public String getNamaperusahaan() {
-        return this.namaperusahaan;
+
+    public String getNama_perusahaan() {
+        return this.nama_perusahaan;
     }
 
-    public void setNamaperusahaan(String namaperusahaan) {
-        this.namaperusahaan = namaperusahaan;
+    public void setNama_perusahaan(String nama_perusahaan) {
+        this.nama_perusahaan = nama_perusahaan;
     }
 
-    public String getAlamatperusahaan() {
-        return this.alamatperusahaan;
+    public String getAlamat_perusahaan() {
+        return this.alamat_perusahaan;
     }
 
-    public void setAlamatperusahaan(String alamatperusahaan) {
-        this.alamatperusahaan = alamatperusahaan;
+    public void setAlamat_perusahaan(String alamat_perusahaan) {
+        this.alamat_perusahaan = alamat_perusahaan;
     }
 
-    public String getDataperusahaan() {
-        return this.dataperusahaan;
+    public String getData_perusahaan() {
+        return this.data_perusahaan;
     }
 
-    public void setDataperusahaan(String dataperusahaan) {
-        this.dataperusahaan = dataperusahaan;
+    public void setData_perusahaan(String data_perusahaan) {
+        this.data_perusahaan = data_perusahaan;
     }
 
     public String getPekerja() {
@@ -47,5 +48,6 @@ public class Perusahaan {
     public void setInvestor(String investor) {
         this.investor = investor;
     }
+    
     
 }
