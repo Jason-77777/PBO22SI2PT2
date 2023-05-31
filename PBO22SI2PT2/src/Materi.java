4ac8fa9ebd6b772da059eb36eb70f65f3396dd77
Jason-77777 <126946180+Jason-77777@users.noreply.github.com>
2023-05-31 20:18:21 +0700

    materi push
    ada setter getter

--- PBO22SI2PT2/src/Materi.java
+++ PBO22SI2PT2/src/Materi.java
@@ -1,8 +1,49 @@
 //class materi adalah class untuk menyimpan data materi yang diperlukan oleh pekerja untuk belajar.
 //isinya biasa menyangkut tentang pertemuan materi, isi materi, jenis materi, kuriikulum mateeri
 
+
+
 public class Materi {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Ini projek PBO, halo nama saya jason");
+    private String pertemuan;
+    private String kurikulum;
+    private String jenis;
+    private String latihan;
+    
+
+    public String getPertemuan() {
+        return this.pertemuan;
     }
-}
+
+    public void setPertemuan(String pertemuan) {
+        this.pertemuan = pertemuan;
+    }
+
+    public String getKurikulum() {
+        return this.kurikulum;
+    }
+
+    public void setKurikulum(String kurikulum) {
+        this.kurikulum = kurikulum;
+    }
+
+    public String getJenis() {
+        return this.jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getLatihan() {
+        return this.latihan;
+    }
+
+    public void setLatihan(String latihan) {
+        this.latihan = latihan;
+    }
+
+
+    
+
+
+    
