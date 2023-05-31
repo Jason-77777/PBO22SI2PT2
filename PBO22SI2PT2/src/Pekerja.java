ee8abf2d3705c401d60e9fb000fe6cfa6c0e8029
cherryxcok <cherryxcok@gmail.com>
2023-05-31 20:49:23 +0700

    sesi 4 cherry cok
    pekerja

--- PBO22SI2PT2/src/Pekerja.java
+++ PBO22SI2PT2/src/Pekerja.java
@@ -2,4 +2,42 @@
 // isinya adalah data yang menyangkut pekerja seperti data diri, alamat, dll
 
 public class Pekerja {
+    private String nama_Id;
+    private String biodata;
+    private String nomor_handphone;
+    private String alamat;
+
+
+    public String getNama_Id() {
+        return this.nama_Id;
+    }
+
+    public void setNama_Id(String nama_Id) {
+        this.nama_Id = nama_Id;
+    }
+
+    public String getBiodata() {
+        return this.biodata;
+    }
+
+    public void setBiodata(String biodata) {
+        this.biodata = biodata;
+    }
+
+    public String getNomor_handphone() {
+        return this.nomor_handphone;
+    }
+
+    public void setNomor_handphone(String nomor_handphone) {
+        this.nomor_handphone = nomor_handphone;
+    }
+
+    public String getAlamat() {
+        return this.alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
 }
