8f12008e50e19f3d6276a00357e848836800a857
cherryxcok <cherryxcok@gmail.com>
2023-06-05 21:05:18 +0700

    Cherry Cok sesi 5 init
    Cherry Cok sesi 5 init

--- PBO22SI2PT2/src/App.java
+++ PBO22SI2PT2/src/App.java
@@ -15,5 +15,8 @@ public class App {
         Pekerja jim = new Pekerja("123", "laki-laki", "085381633", "jl murai no 2 blok 1");
         Pekerja jack = new Pekerja("126", "laki-laki", "082739264", "cemara garuda no 3");
         
-    }
+        Materi pertama = new Materi("123", "2023", "latihan", "permulaan");
+        Materi kedua = new Materi("234", "2023", "workshop", "advance");
+        Materi ketiga = new Materi("456", "2022", "latihan", "advance");
+    }   
 }
