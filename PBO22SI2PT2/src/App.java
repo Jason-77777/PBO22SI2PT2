298921bcf50059088fe88b14a719d4521bc0f22f
Jason-77777 <126946180+Jason-77777@users.noreply.github.com>
2023-06-13 23:19:01 +0700

    Update App.java

--- PBO22SI2PT2/src/App.java
+++ PBO22SI2PT2/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Menu utama :");
         System.out.println("1. Fitur Profile Pekerja dan Perusahaan");
-        System.out.println("2. Fitur Kualifikasi");
+        System.out.println("2. Fitur Chat");
         System.out.println("3. Fitur Training");
         System.out.println("4. Fitur Pencarian Pekerjaan");
         System.out.println("5. Pesan dan Notifikasi");
