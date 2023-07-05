431ba87e1504e7ecb25cedd059d9cdb947b014a8
VickyDarmana <134504626+VickyDarmana@users.noreply.github.com>
2023-07-05 20:18:41 +0700

    tes
    tesjhg

--- Kelompok 5/src/LowonganPekerjaan.java
+++ Kelompok 5/src/LowonganPekerjaan.java
@@ -1,89 +1,14 @@
 public class LowonganPekerjaan {
 
+    private String idLowongan;
+    private String idPerusahaan;
     private String namaPerusahaan;
     private String jobdesc;
     private String jumlahPekerja;
-    private String jenisPekerjaan;
+    private JenisPekerjaan jenisPekerjaan;
     private String lokasi;
     private String lulusan;
 
-    public LowonganPekerjaan() {
-    }
-
-
-    public LowonganPekerjaan(String namaPerusahaan, String jobdesc, String jumlahPekerja, String jenisPekerjaan, String lokasi, String lulusan) {
-        this.namaPerusahaan = namaPerusahaan;
-        this.jobdesc = jobdesc;
-        this.jumlahPekerja = jumlahPekerja;
-        this.jenisPekerjaan = jenisPekerjaan;
-        this.lokasi = lokasi;
-        this.lulusan = lulusan;
-    }
-
-
-    public String getNamaPerusahaan() {
-        return this.namaPerusahaan;
-    }
-
-    public void setNamaPerusahaan(String namaPerusahaan) {
-        this.namaPerusahaan = namaPerusahaan;
-    }
-
-    public String getJobdesc() {
-        return this.jobdesc;
-    }
-
-    public void setJobdesc(String jobdesc) {
-        this.jobdesc = jobdesc;
-    }
-
-    public String getJumlahPekerja() {
-        return this.jumlahPekerja;
-    }
-
-    public void setJumlahPekerja(String jumlahPekerja) {
-        this.jumlahPekerja = jumlahPekerja;
-    }
-
-    public String getJenisPekerjaan() {
-        return this.jenisPekerjaan;
-    }
-
-    public void setJenisPekerjaan(String jenisPekerjaan) {
-        this.jenisPekerjaan = jenisPekerjaan;
-    }
-
-    public String getLokasi() {
-        return this.lokasi;
-    }
-
-    public void setLokasi(String lokasi) {
-        this.lokasi = lokasi;
-    }
-
-    public String getLulusan() {
-        return this.lulusan;
-    }
-
-    public void setLulusan(String lulusan) {
-        this.lulusan = lulusan;
-    }
-
-
-    @Override
-    public String toString() {
-        return "{" +
-            "Nama perusahaan = '" + getNamaPerusahaan() + "'" +
-            ", Job description = '" + getJobdesc() + "'" +
-            ", Jumlah pekerja yang dibutuhkan = '" + getJumlahPekerja() + "'" +
-            ", Tipe pekerjaan = '" + getJenisPekerjaan() + "'" +
-            ", Lokasi = '" + getLokasi() + "'" +
-            ", Lulusan = '" + getLulusan() + "'" +
-            "}";
-    }
-
-
-
-
+   
     
 }
